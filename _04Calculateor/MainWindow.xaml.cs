fc6b36b3eea129c62ac1f9a2bb4bea0ccf797ec7
RoeiBarak-intel <110981463+RoeiBarak-intel@users.noreply.github.com>
2022-09-05 13:52:13 +0300

    Add files via upload

---
+++ _04Calculateor/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04Calculateor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnNumberButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button clicked = (Button)sender;
+            txbInput.Text = txbInput.Text + clicked.Content;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txbInput.Text;
+            int number = 0;
+            int firstNumber = 0;
+            char operation = ' ';
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ('0' <= input[i] && input[i] <= '9')
+                {
+                    number = number * 10;
+                    number = number + (input[i] - '0');
+                }
+                else if (input[i] == '+' ||
+                    input[i] == '-' ||
+                    input[i] == '/' ||
+                    input[i] == '*')
+                {
+                    switch (operation)
+                    {
+                        case '+':
+                            number = firstNumber + number;
+                            break;
+                        case '-':
+                            number = firstNumber - number;
+                            break;
+                        case '/':
+                            number = firstNumber / number;
+                            break;
+                        case '*':
+                            number = firstNumber * number;
+                            break;
+                    }
+                    operation = input[i];
+                    firstNumber = number;
+                    number = 0;
+                }
+            }
+            switch (operation)
+            {
+                case '+':
+                    number = firstNumber + number;
+                    break;
+                case '-':
+                    number = firstNumber - number;
+                    break;
+                case '*':
+                    number = firstNumber * number;
+                    break;
+                case '/':
+                    number = firstNumber / number;
+                    break;
+            }
+            txbInput.Text = number.ToString();
+            }
+        
+    }
+}
---
+++ _05TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if(fd.ShowDialog() == true)
+            {
+                StreamReader reader = new StreamReader(fd.FileName);
+                txtBox.Document.Blocks.Clear();
+                txtBox.AppendText(reader.ReadToEnd());
+                reader.Close();
+            }
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            if (sf.ShowDialog() == true)
+            {
+                StreamWriter writer = new StreamWriter(sf.FileName);
+                string text = new TextRange(txtBox.Document.ContentStart,
+                                            txtBox.Document.ContentEnd).Text;
+                writer.Write(text);
+                writer.Close();
+            }
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
