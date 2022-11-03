44084efc82ee0c58e500e07f5b8f580e14c4d9ba
RoeiBarak-intel <110981463+RoeiBarak-intel@users.noreply.github.com>
2022-11-03 11:50:18 +0200

    _10

---
+++ _10Tick-Tck-Toe/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10Tick_Tck_Toe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Boolean cross = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Canvas cnv = (Canvas)sender;
+            if (cross)
+            {
+                int padding = 15;
+                Line ln = new Line();
+                ln.X1 = padding;
+                ln.Y1 = padding;
+                ln.X2 = cnv.Width - padding;
+                ln.Y2 = cnv.Height - padding;
+                ln.Stroke = Brushes.Blue;
+                ln.StrokeThickness = 10;
+                cnv.Children.Add(ln);
+                Line ln2 = new Line();
+                ln2.X1 = padding;
+                ln2.Y1 = cnv.Height - padding;
+                ln2.X2 = cnv.Width - padding;
+                ln2.Y2 = padding;
+                ln2.Stroke = Brushes.Blue;
+                ln2.StrokeThickness = 10;
+                cnv.Children.Add(ln2);
+            }
+            else
+            {
+                Ellipse el = new Ellipse();
+                el.Width = 80;
+                el.Height = 80;
+                el.Margin = new Thickness(10, 10, 10, 10);
+                el.Fill = Brushes.Transparent;
+                el.Stroke = Brushes.Red;
+                el.StrokeThickness = 15;
+                cnv.Children.Add(el);
+
+
+            }
+            cross = !cross;
+        }
+    }
+}
